eb608f147e0029391ed89d677055e67f7f9d0215
nerdfiles <nerdfiles@gmail.com>
2014-05-21 10:39:16 -0500

    Using Yeoman generator-coffee.

---
+++ out/lib/nggeotranslation.js
@@ -0,0 +1,17 @@
+
+/*
+
+ng.geotranslation
+https://github.com/nerdfiles/nggeotranslation
+
+Copyright (c) 2014 nerdfiles
+Licensed under the WTFPL license.
+ */
+
+(function() {
+  'use strict';
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}).call(this);
---
+++ src/lib/nggeotranslation.coffee
@@ -0,0 +1,14 @@
+###
+
+ng.geotranslation
+https://github.com/nerdfiles/nggeotranslation
+
+Copyright (c) 2014 nerdfiles
+Licensed under the WTFPL license.
+
+###
+
+'use strict'
+
+exports.awesome = ()->
+  'awesome'
